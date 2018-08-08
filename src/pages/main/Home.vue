6babd52d1c350f69098c1e39be8d33dc29ac2c3e
luozhuoqun <13821744963@163.com>
2018-08-09 06:34:11 +0800

    完成项目crud前后端交互逻辑

--- src/pages/main/Home.vue
+++ src/pages/main/Home.vue
@@ -1,22 +1,36 @@
 <template>
-  <div>
-    <ProItem @addPro="addProject">
-    </ProItem>
+  <div id="project">
+    <el-button type="primary" @click="addProject">创建项目</el-button>
 
-      <el-dialog title="编辑项目" :visible.sync="dialogFormVisible">
-        <el-form ref="form" :model="form" label-width="80px">
-          <el-form-item label="项目名称">
-            <el-input v-model="editProject.projectName"></el-input>
-          </el-form-item>
-          <el-form-item label="描述">
-            <el-input type="textarea" v-model="editProject.description"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="savePro">保存</el-button>
-            <el-button @click="cancelPro">取消</el-button>
-          </el-form-item>
-        </el-form>
-      </el-dialog>
+    <el-dialog title="创建项目" :visible.sync="pro_isShow">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="项目名称">
+          <el-input type="textarea" v-model="project.proName"></el-input>
+        </el-form-item>
+        <el-form-item label="描述">
+          <el-input type="textarea" v-model="project.proDescription"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button @click="cancelPro">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+
+    <el-dialog title="编辑项目" :visible.sync="dialogFormVisible">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="项目名称">
+          <el-input v-model="editProject.proName"></el-input>
+        </el-form-item>
+        <el-form-item label="描述">
+          <el-input type="textarea" v-model="editProject.proDescription"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="savePro()">保存</el-button>
+          <el-button @click="cancelPro">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
 
     <el-table
       :data="projects"
@@ -26,7 +40,7 @@
         width="180">
         <template slot-scope="scope">
           <i class="el-icon-time"></i>
-          <span style="margin-left: 10px">{{ scope.row.projectName }}</span>
+          <span style="margin-left: 10px">{{ scope.row.proName }}</span>
         </template>
       </el-table-column>
       <el-table-column
@@ -34,31 +48,37 @@
         width="180">
         <template slot-scope="scope">
           <el-popover trigger="hover" placement="top">
-            <p>: {{ scope.row.time }}</p>
-            <p>描述: {{ scope.row.description }}</p>
+            <p>: {{ scope.row.proTime }}</p>
+            <p v-show="false">: {{scope.row.userId}}</p>
+            <p v-show="false">: {{scope.row.proId}}</p>
+            <p>描述: {{ scope.row.proDescription }}</p>
             <div slot="reference" class="name-wrapper">
-              <el-tag size="medium">{{ scope.row.time }}</el-tag>
+              <el-tag size="medium">{{ scope.row.proTime }}</el-tag>
             </div>
           </el-popover>
         </template>
       </el-table-column>
+
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button
-            size="mini"
-            type="info"
-            @click="manageProjectFile(scope.$index, scope.row)">管理项目文件</el-button>
+          <!--<el-button-->
+          <!--size="mini"-->
+          <!--type="info"-->
+          <!--@click="manageProjectFile(scope.$index, scope.row)">管理项目文件</el-button>-->
           <el-button
             size="mini"
             type="primary"
-            @click="processVideo(scope.$index, scope.row)">行人轨迹检测</el-button>
+            @click="processVideo(scope.$index, scope.row)">行人轨迹检测
+          </el-button>
           <el-button
             size="mini"
-            @click="handleEdit(scope.$index, scope.row)">编辑信息</el-button>
+            @click="handleEdit(scope.$index, scope.row)">编辑信息
+          </el-button>
           <el-button
             size="mini"
             type="danger"
-            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            @click="handleDelete(scope.$index, scope.row)">删除
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -67,61 +87,157 @@
 </template>
 
 <script>
-  import ProItem from '../../components/item/projectItem.vue'
   import ElDialog from "../../../node_modules/element-ui/packages/dialog/src/component.vue";
+  import axios from 'axios'
+  import $ from 'jquery'
+  import {timestamp2Date,Date2timestamp} from '../../../src/util/fmtDate.js'
+
   export default {
-   components:{
-     ElDialog,
-     ProItem
-   },
+    name: 'form',
+    components: {
+      ElDialog
+    },
     data() {
       return {
-        dialogFormVisible:false,
-        editProject:{},
-        editIndex:"",
-        projects: []
+        projects: [],
+        pro_isShow: false,
+        dialogFormVisible: false,
+        project: {
+          proName: '',
+          proId: '',
+          proTime: '',
+          proDescription: '',
+          userId: ''
+        },
+        editProject: {
+          proName: '',
+          proId: '',
+          proTime: '',
+          proDescription: '',
+          userId: ''
+        },
+        editIndex: "",
+
       }
     },
+    mounted: function () {
+      var _this = this;
+      _this.$axios.post('http://localhost:8081/project/getProjectsByUserId', {
+        userId: 1
+      }).then(function (response) {
+        _this.projects = response.data
+        _this.projects.forEach(function (value, index, array) {
+          array[index].proTime = timestamp2Date(value.proTime)
+          console.log(array[index].proTime)
+        })
+      })
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
     methods: {
-      savePro(index,editProject){
-        this.dialogFormVisible=false,
-         this.projects[index] = editProject
+      addProject() {
+        this.pro_isShow = true,
+          this.project = {}
+        localStorage.setItem('userId', 1)
+        this.project.userId = localStorage.getItem('userId')
       },
-      cancelPro(){
-        this.dialogFormVisible = false
-      },
-      handleEdit(index, row) {
-        this.dialogFormVisible = true,
-        this.editProject = row
-        this.index = index
-      },
-      addProject(e){
-        if(this.projects.length>4)
+      onSubmit() {
+        this.pro_isShow = false
+        console.log(this.project)
+        this.dialogFormVisible = false,
+          console.log(this.project)
+        if (this.projects.length > 4)
           alert("您最多创建5个项目")
         else {
-          if(e.projectName.trim()) {
+          if (this.project.proName.trim()) {
             this.projects.unshift({
-              projectName: e.projectName.trim(),
-              description:e.description,
-              time: new Date()
+              proName: this.project.proName.trim(),
+              proDescription: this.project.proDescription,
+              proTime: new Date(),
+              userId: this.project.userId
             });
-            e.projectName = '';
+            this.project.proName = '';
           } else {
             alert('项目名不能为空 !-_-');
           }
+          axios.post('http://localhost:8081/project/createProject', {
+            project: this.projects[0]
+          }).then(function (response) {
+            console.log(response.data)
+          })
+            .catch(function (error) {
+              console.log(error)
+            })
         }
       },
+      cancelPro() {
+        this.pro_isShow = false
+
+      },
+      savePro(index,editProject){
+        this.dialogFormVisible=false,
+        this.projects[index] = editProject
+        var _this = this
+        this.dialogFormVisible = false
+        this.editProject.proTime = new Date()
+        console.log(this.projects[this.editIndex])
+        axios.post('http://localhost:8081/project/updateProject', {
+          project: _this.editProject
+        }).then(function (response) {
+          console.log(response.data)
+
+        })
+          .catch(function (error) {
+            console.log(error)
+          })
+      },
+      cancelPro() {
+        this.dialogFormVisible = false
+      },
+      handleEdit(index, row) {
+        var _this = this
+        this.editIndex = index
+        this.editProject = row
+        this.dialogFormVisible = true
+        $.ajax({
+          type: 'POST',
+          url: "http://localhost:8081/project/getProjectByProjectId",
+          data: JSON.stringify({proId: row.proId}),
+          dataType: "JSON",
+          contentType: 'application/json;charset=utf-8',
+          success: function (data) {
+//            _this.editIndex = index
+//            _this.editProject = data
+          },
+          error: function (jqXHR) {
+            return "哎呀呀，出错了" + jqXHR.status;
+          }
+        });
+
+      },
+
       handleDelete(index, row) {
         this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
-          this.projects.splice(this.projects.findIndex(project => project.index === index), 1),
-          this.$message({
-            type: 'success',
-            message: '删除成功!'
-          });
+          this.projects.splice(index, 1),
+            $.ajax({
+              type: 'POST',
+              url: "http://localhost:8081/project/deleteProject",
+              data: JSON.stringify({proId: row.proId}),
+              dataType: "JSON",
+              contentType: 'application/json;charset=utf-8',
+              error: function (jqXHR) {
+                return "哎呀呀，出错了" + jqXHR.status;
+              }
+            });
+            this.$message({
+              type: 'success',
+              message: '删除成功!'
+            });
         }).catch(() => {
           this.$message({
             type: 'info',
---
+++ src/util/fmtDate.js
@@ -0,0 +1,16 @@
+export {timestamp2Date,Date2timestamp}
+function timestamp2Date(timestamp) {
+  var date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+  var Y = date.getFullYear() + '-';
+  var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+  var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + ' ';
+  var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+  var m = (date.getMinutes() <10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+  var s = (date.getSeconds() <10 ? '0' + date.getSeconds() : date.getSeconds());
+  return Y+M+D+h+m+s;
+};
+function Date2timestamp(str) {
+  var date = new Date(str);
+  // 有三种方式获取
+  return date.getTime();
+}
