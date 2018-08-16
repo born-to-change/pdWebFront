c80b7dd5885d69f4a938bd286de999d8c1a2368a
luozhuoqun <13821744963@163.com>
2018-08-16 15:43:13 +0800

    添加camera的crud

--- src/components/map/map.vue
+++ src/components/map/map.vue
@@ -26,12 +26,15 @@
     <el-dialog
       title="提示"
       :visible.sync="editCameraDialogVisible"
+      :before-close="closeCamDialog"
       width="60%"
       center>
       <el-form :model="cam">
         <el-form-item label="摄像头名称" :label-width="formLabelWidth">
           <el-input v-model="cam.camName" auto-complete="off"></el-input>
           <el-input v-show="false" v-model="cam.videoUrl"></el-input>
+          <el-input v-show="false" v-model="editOrCreate"></el-input>
+          <el-input v-show="false" v-model="cam.cameraId"></el-input>
         </el-form-item>
         <el-form-item label="描述" :label-width="formLabelWidth">
           <el-input v-model="cam.cameraDescription" placeholder="请填写摄像头备注"></el-input>
@@ -62,6 +65,53 @@
     <el-button type="primary" @click="saveCamera">确 定</el-button>
   </span>
     </el-dialog>
+
+    <el-table
+      :data="cameras"
+      style="width: 100%">
+      <el-table-column
+        label="摄像头名称"
+        width="180">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ scope.row.camName }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="时间"
+        width="180">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>: {{ scope.row.proTime }}</p>
+            <p v-show="false">: {{scope.row.userId}}</p>
+            <p v-show="false">: {{scope.row.proId}}</p>
+            <p>描述: {{ scope.row.cameraDescription }}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.videoTime }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="primary"
+            @click="processCamera(scope.$index, scope.row)">处理视频
+          </el-button>
+          <el-button
+            size="mini"
+            @click="editCamera(scope.$index, scope.row)">编辑
+          </el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="deleteCamera(scope.$index, scope.row)">删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 
 </template>
@@ -69,9 +119,12 @@
 <script>
   import mapLocate from './index.vue'
   import axios from 'axios'
+  import ElButton from "../../../node_modules/element-ui/packages/button/src/button.vue";
+  import {timestamp2Date,Date2timestamp} from '../../../src/util/fmtDate.js'
   export default {
     name: 'amap-page',
     components: {
+      ElButton,
       mapLocate
     },
     data() {
@@ -79,6 +132,8 @@
         fileId:"",
         fileName: '',
         videos:[],
+        editOrCreate:1,
+        editCamId:"",
         isEditCamera:false,
         editCameraDialogVisible: false,
         count: 1,
@@ -91,7 +146,9 @@
         zoom: 14,
         center: [116.405306, 39.904989],
         markers: [],
+        cameras:[],
         cam:{
+          cameraId:"",
           videoImage:"Na",
           cameraDescription:"",
           camLng:"Na",
@@ -130,9 +187,77 @@
         }
       };
     },
+    mounted: function () {
+      var _this = this;
+      axios.post('http://172.18.32.192:8081/camera/getCamerasByProId', {
+        proId: localStorage.getItem("proId")
+      }).then(function (response) {
+        _this.cameras = response.data
+      })
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
     methods: {
+      closeCamDialog(){
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            this.editCameraDialogVisible = false
+            this.cam ={videoImage:"Na"}
+            done();
+          })
+          .catch(_ => {});
+      },
+      processCamera(){
+
+      },
+      deleteCamera(){
+
+      },
+      editCamera(index,row){
+        var _this = this;
+        axios.post('http://172.18.32.192:8081/camera/getCamByBingingFileId', {
+          bingingFileId: row.bingingFileId,
+          proId:parseInt(localStorage.getItem("proId"))
+        }).then(function (response) {
+          var temp = response.data
+          _this.cam = temp
+          _this.editCameraDialogVisible = true
+          _this.editOrCreate = 0
+          _this.editCamId = temp.cameraId
+          _this.fileId = temp.bingingFileId
+
+        })
+        axios.post('http://172.18.32.192:8081/file/getFileByFileId', {
+          fileId: this.fileId
+        }).then(function (response) {
+          var temp = response.data
+          _this.cam.videoUrl = temp.fileUrl
+          _this.fileName = temp.fileName
+
+        })
+          .catch(function (error) {
+            console.log(error)
+          })
+
+          .catch(function (error) {
+            console.log(error)
+          })
+        var _this = this
+        axios.post('http://172.18.32.192:8081/file/getVideosByUserId', {
+          userId: localStorage.getItem("userId"),
+          proId:localStorage.getItem("proId")
+        }).then(function (response) {
+          var files = response.data
+          _this.videos = files
+        })
+          .catch(function (error) {
+            console.log(error)
+          })
+      },
       cancelCamera(){
         this.editCameraDialogVisible = false
+        this.markers.splice(0,1);
       },
       bindingVideo(){
         var _this = this
@@ -148,20 +273,37 @@
           })
       },
       saveCamera(){
+        var _this = this;
         this.editCameraDialogVisible = false
         this.cam.bingingFileId = this.fileId
-        axios.post('http://172.18.32.192:8081/camera/createCamera', {
-          camera: this.cam
-        }).then(function (response) {
-
-        })
-          .catch(function (error) {
-            console.log(error)
+        if(this.editOrCreate == 1) {
+          axios.post('http://172.18.32.192:8081/camera/createCamera', {
+            camera: _this.cam
+          }).then(function (response) {
+            _this.cameras.splice(0, 0, _this.cam)
           })
+            .catch(function (error) {
+              console.log(error)
+            })
+        }else {
+          this.cam.cameraId = _this.editCamId
+          this.cam.videoTime = timestamp2Date( this.cam.videoTime)
+          console.log(this.cam.videoTime)
+          axios.post('http://172.18.32.192:8081/camera/updateCamera', {
+            camera: _this.cam
+          }).then(function (response) {
+            _this.cameras.splice(0, 0, _this.cam)
+          })
+            .catch(function (error) {
+              console.log(error)
+            })
+        }
 
       },
       createCamera(){
         this.isEditCamera = true
+        this.cam ={videoImage:"Na"}
+        this.editOrCreate = 1
       },
       onClick() {
         this.count += 1;
@@ -174,7 +316,8 @@
       addMarker:function (dragData) {
         var _this = this
         axios.post('http://172.18.32.192:8081/file/getVideosByUserId', {
-          userId: localStorage.getItem("userId")
+          userId: localStorage.getItem("userId"),
+          proId:localStorage.getItem("proId")
         }).then(function (response) {
           var files = response.data
           _this.videos = files
